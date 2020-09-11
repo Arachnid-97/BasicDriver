3305f5982cc4ea44dcf38560e917271e025fa617
liziyuan <liziyuan970528@163.com>
2020-09-11 20:58:02 +0800

    update nrf24l01 code

--- NRF24L01/nrf24l01_drv.c
+++ NRF24L01/nrf24l01_drv.c
@@ -2,6 +2,10 @@
 #include "bsp_uart.h"
 
 
+#define NRF_DYNAMIC_PACKET		0
+
+#define TIME_DELAY				10
+
 /************************************************
 函数名称 ： NRF24L_Delay_us
 功    能 ： 软件毫秒延时
@@ -10,10 +14,10 @@
 *************************************************/
 static void NRF24L_Delay_us( uint32_t Count )
 {
-	while(Count)
-	{
-		Count--;
-	}
+    while(Count)
+    {
+        Count--;
+    }
 }
 
 /************************************************
@@ -34,91 +38,90 @@ static void NRF24L_Delay_ms( uint32_t Count )
 
 /************************************************
 函数名称 ： NRF24L_Data_RW
-功    能 ： NRF24L01数据读写 
-参    数 ： Data ---- 数据 
+功    能 ： NRF24L01数据读写
+参    数 ： Data ---- 数据
 返 回 值 ： Data ---- 数据
 *************************************************/
 static uint8_t NRF24L_Data_RW( uint8_t Data )
 {
-	uint8_t i;
-	
-	NRF_SCK(LOW);
-	for(i = 0;i < 8;i++)
-	{
-		if(Data & 0x80)
-		{
-			NRF_MOSI(HIGH);
-		}
-		else
-		{
-			NRF_MOSI(LOW);
-		}
-		NRF24L_Delay_us(1);
-		Data <<= 1;
-		NRF_SCK(HIGH);
-		NRF24L_Delay_us(1);
-		Data |= NRF_MISO;
-		NRF_SCK(LOW);
-		NRF24L_Delay_us(1);
-	}
-	
-//	UART_SendByte(Data);
-	return Data;
+    uint8_t i;
+
+    NRF_SCK(LOW);
+    for(i = 0; i < 8; i++)
+    {
+        if(Data & 0x80)
+        {
+            NRF_MOSI(HIGH);
+        }
+        else
+        {
+            NRF_MOSI(LOW);
+        }
+        NRF24L_Delay_us(3);
+        Data <<= 1;
+        NRF_SCK(HIGH);
+        NRF24L_Delay_us(3);
+        Data |= NRF_MISO;
+        NRF_SCK(LOW);
+        NRF24L_Delay_us(3);
+    }
+
+    return Data;
 }
 
 /************************************************
 函数名称 ： NRF24L_Write_RegByte
-功    能 ： NRF24L01写寄存器一个字节 
+功    能 ： NRF24L01写寄存器一个字节
 参    数 ： Address ---- 地址
-			Value ---- 输入值 
+			Value ---- 输入值
 返 回 值 ： 无
 *************************************************/
 static void NRF24L_Write_RegByte( uint8_t Address, uint8_t Value )
-{	
-	NRF_CE(LOW);
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(WRITE_REG | Address);
-	NRF24L_Data_RW(Value);
-	NRF_CSN(HIGH);
+{
+    NRF_CE(LOW);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_WRITE_REG | Address);
+    NRF24L_Data_RW(Value);
+    NRF_CSN(HIGH);
 }
 
 /************************************************
 函数名称 ： NRF24L_Read_RegByte
-功    能 ： NRF24L01读寄存器一个字节 
-参    数 ： Address ---- 地址	
-返 回 值 ： value ---- 输入值 
+功    能 ： NRF24L01读寄存器一个字节
+参    数 ： Address ---- 地址
+返 回 值 ： value ---- 输入值
 *************************************************/
 static uint8_t NRF24L_Read_RegByte( uint8_t Address )
-{	
-	uint8_t value = 0;
-	 
-	NRF_CE(LOW);
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(READ_REG | Address);
-	value = NRF24L_Data_RW(NOP);
-	NRF_CSN(HIGH);
-	
-	return value;
+{
+    uint8_t value = 0;
+
+    NRF_CE(LOW);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_READ_REG | Address);
+    value = NRF24L_Data_RW(NRF_NOP);
+    NRF_CSN(HIGH);
+
+    return value;
 }
 
 /************************************************
 函数名称 ： NRF24L_Write_RegnByte
-功    能 ： NRF24L01写寄存器多个字节（最大 5字节宽度） 
+功    能 ： NRF24L01写寄存器多个字节（最大 5字节宽度）
 参    数 ： Address ---- 地址
-			pData ---- 输入值 
-			Len ---- 字节数 
+			pData ---- 输入值
+			Len ---- 字节数
 返 回 值 ： 无
 *************************************************/
-static void NRF24L_Write_RegnByte( uint8_t Address, const uint8_t *pData ,uint8_t Len )
-{	
-	NRF_CE(LOW);
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(WRITE_REG | Address);
-	while(Len--)
-	{
-		NRF24L_Data_RW(*pData++);
-	}	
-	NRF_CSN(HIGH);
+static void NRF24L_Write_RegnByte( uint8_t Address, const uint8_t *pData,uint8_t Len )
+{
+    NRF_CE(LOW);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_WRITE_REG | Address);
+    while(Len--)
+    {
+        NRF24L_Data_RW(*pData++);
+    }
+    NRF_CSN(HIGH);
 }
 
 /************************************************
@@ -131,15 +134,15 @@ static void NRF24L_Write_RegnByte( uint8_t Address, const uint8_t *pData ,uint8_
 *************************************************/
 static void NRF24L_Read_RegnByte( uint8_t Address, uint8_t *pData, uint8_t Len )
 {
-	NRF_CE(LOW);
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(READ_REG | Address);
-	while (Len--)
-	{
-		*pData = NRF24L_Data_RW(NOP);
-		pData++;
-	}
-	NRF_CSN(HIGH);
+    NRF_CE(LOW);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_READ_REG | Address);
+    while (Len--)
+    {
+        *pData = NRF24L_Data_RW(NRF_NOP);
+        pData++;
+    }
+    NRF_CSN(HIGH);
 }
 
 /************************************************
@@ -151,13 +154,13 @@ static void NRF24L_Read_RegnByte( uint8_t Address, uint8_t *pData, uint8_t Len )
 *************************************************/
 void NRF24L_Write_Buf( const uint8_t *pBuf, uint8_t Len )
 {
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(WR_TX_PLOAD);
-	while (Len--)
-	{
-		NRF24L_Data_RW(*pBuf++);
-	}
-	NRF_CSN(HIGH);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_WR_TX_PLOAD);
+    while (Len--)
+    {
+        NRF24L_Data_RW(*pBuf++);
+    }
+    NRF_CSN(HIGH);
 }
 
 /************************************************
@@ -169,14 +172,14 @@ void NRF24L_Write_Buf( const uint8_t *pBuf, uint8_t Len )
 *************************************************/
 void NRF24L_Read_Buf( uint8_t *pBuf, uint8_t Len )
 {
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(WR_TX_PLOAD);
-	while (Len--)
-	{
-		*pBuf = NRF24L_Data_RW(NOP);
-		pBuf++;
-	}
-	NRF_CSN(HIGH);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_WR_TX_PLOAD);
+    while (Len--)
+    {
+        *pBuf = NRF24L_Data_RW(NRF_NOP);
+        pBuf++;
+    }
+    NRF_CSN(HIGH);
 }
 
 /************************************************
@@ -187,9 +190,9 @@ void NRF24L_Read_Buf( uint8_t *pBuf, uint8_t Len )
 *************************************************/
 static void NRF24L_Clean_TX_Flush(void)
 {
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(FLUSH_TX);
-	NRF_CSN(HIGH);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_FLUSH_TX);
+    NRF_CSN(HIGH);
 }
 
 /************************************************
@@ -200,9 +203,9 @@ static void NRF24L_Clean_TX_Flush(void)
 *************************************************/
 static void NRF24L_Clean_RX_Flush(void)
 {
-	NRF_CSN(LOW);
-	NRF24L_Data_RW(FLUSH_RX);
-	NRF_CSN(HIGH);
+    NRF_CSN(LOW);
+    NRF24L_Data_RW(NRF_FLUSH_RX);
+    NRF_CSN(HIGH);
 }
 
 /************************************************
@@ -216,60 +219,53 @@ static void NRF24L_Clean_RX_Flush(void)
 *************************************************/
 uint8_t NRF24L_SendPacket( const uint8_t *pData, uint8_t Const )
 {
-	uint8_t state = 0x70;
+    uint8_t state = 0x70;
 
-	if((Const > 0) && (Const <= TX_PLOAD_WIDTH))
-	{
-		NRF_CE(LOW);
-		NRF24L_Write_RegByte(CONFIG, 0x0E);				// 转换成发送模式
-	//	NRF_CE(HIGH);
-	//	NRF24L_Delay_us(10);
-	//	NRF_CE(LOW);									// CE拉低，使能24L01配置
-		NRF24L_Write_Buf(pData, Const);					// 写数据到TX BUF
-		NRF_CE(HIGH);
-		NRF24L_Delay_us(10);
-		while(NRF_IRQ)									// 等待发送结束
-		{
-			if(0xFF == state)
-			{
-				break;
-			}
-			state++;
-		}
-		NRF_CE(LOW);
-		state = NRF24L_Read_RegByte(STATUS);			// 读取状态寄存器的值
-		NRF24L_Write_RegByte(STATUS, state);			// 清除 TX_DS或 MAX_RT中断标志
-	//	NRF_CE(HIGH);
-		
-//		UART0_SendByte(state);
-				
-		switch(state & 0x30)
-		{
-			case TX_OK:
-						state = 1;
-						break;
-			case TX_MAX:
-//						NRF24L_Clean_TX_Flush();
-						state = 0xFF;
-						break;
-			default :
-						state = 0;
-						break;
-		}
-		NRF24L_Clean_TX_Flush();						// 清缓存
-		
-		NRF24L_Write_RegByte(CONFIG, 0x0F);				// 发送完成，转换成接收模式
-		
-		NRF_CE(HIGH);
-	}
-	else
-	{
-		state = 0;
-	}
-		
-//	UART0_SendByte(state);
-	
-	return state;
+    if((Const > 0) && (Const <= NRF_TX_PLOAD_WIDTH))
+    {
+        NRF_CE(LOW);
+        NRF24L_Write_RegByte(NRF_CONFIG, 0x0E);				// 转换成发送模式
+        NRF24L_Write_Buf(pData, Const);						// 写数据到TX BUF
+        NRF_CE(HIGH);
+        NRF24L_Delay_us(TIME_DELAY);
+        while(NRF_IRQ)										// 等待发送结束
+        {
+            if(0xFF == state)
+            {
+                break;
+            }
+            state++;
+        }
+        NRF_CE(LOW);
+        state = NRF24L_Read_RegByte(NRF_STATUS);			// 读取状态寄存器的值
+        NRF24L_Write_RegByte(NRF_STATUS, state);			// 清除 TX_DS或 MAX_RT中断标志
+
+        switch(state & 0x30)
+        {
+        case NRF_TX_OK:
+            state = 1;
+            break;
+        case NRF_TX_MAX:
+            state = 0xFF;
+            break;
+        default :
+            state = 0;
+            break;
+        }
+        NRF24L_Clean_TX_Flush();							// 清缓存
+
+        NRF24L_Write_RegByte(NRF_CONFIG, 0x0F);				// 发送完成，转换成接收模式
+
+        NRF_CE(HIGH);
+    }
+    else
+    {
+        state = 0;
+    }
+
+	DEBUG_PRINTF("status=%d", state);
+
+    return state;
 }
 
 /************************************************
@@ -280,32 +276,37 @@ uint8_t NRF24L_SendPacket( const uint8_t *pData, uint8_t Const )
 返 回 值 ： 0 ---- 接收失败
 			1 ---- 接收成功
 *************************************************/
-bit NRF24L_ReceivePacket( uint8_t *pData, uint8_t Const )
+uint8_t NRF24L_ReceivePacket( uint8_t *pData, uint8_t Const )
 {
-	uint8_t state = 0x70;
+    uint8_t state = 0x70;
 
-	NRF24L_Clean_RX_Flush();							// 清缓存
-	
-	if((Const > 0) && (Const <= RX_PLOAD_WIDTH))
-	{
-		NRF_CE(LOW);
-		state = NRF24L_Read_RegByte(STATUS);			// 读取状态寄存器的值
-		NRF24L_Write_RegByte(STATUS, state);			// 清除 RX_DR中断标志
-		if(state & RX_OK)
+    if((Const > 0) && (Const <= NRF_RX_PLOAD_WIDTH))
+    {
+        NRF_CE(LOW);
+        state = NRF24L_Read_RegByte(NRF_STATUS);			// 读取状态寄存器的值
+        NRF24L_Write_RegByte(NRF_STATUS, state);			// 清除 RX_DR中断标志
+        if(state & NRF_RX_OK)
+        {
+            NRF24L_Read_Buf(pData, Const);					// 读出RX BUF的数据
+            NRF24L_Clean_RX_Flush();
+
+//			USART_SendString(DEBUG_UART, pData, Const);
+
+            state = 1;
+        }
+		else
 		{
-			NRF24L_Read_Buf(pData, Const);				// 读出RX BUF的数据
-			NRF24L_Clean_RX_Flush();
-			NRF_CE(HIGH);
-			NRF24L_Delay_us(10);
-			NRF_CE(LOW);
-			
-	//		UART0_SendString(pData, Const);
-			
-			return 1;
+			state = 0;
 		}
+		NRF_CE(HIGH);
+		NRF24L_Delay_us(TIME_DELAY);
+    }
+	else
+	{
+		state = 0;
 	}
-	
-	return 0;
+
+    return state;
 }
 
 /************************************************
@@ -314,34 +315,32 @@ bit NRF24L_ReceivePacket( uint8_t *pData, uint8_t Const )
 参    数 ： 无
 返 回 值 ： 0 / 1
 *************************************************/
-static bit NRF24L_Check(void)
+static uint8_t NRF24L_Check(void)
 {
-	const uint8_t check_in_buf[5] = {0x11,0x22,0x33,0x44,0x55};
-	uint8_t check_out_buf[5] = {0};
+    const uint8_t check_in_buf[5] = {0x11,0x22,0x33,0x44,0x55};
+    uint8_t check_out_buf[5] = {0};
 
-//	NRF_SCK(LOW);
-//	NRF_CSN(HIGH);
-	NRF_CE(LOW);
+    NRF_CE(LOW);
 
-	NRF24L_Write_RegnByte(TX_ADDR, check_in_buf, NRF_ADDR_WIDTH);
+    NRF24L_Write_RegnByte(NRF_TX_ADDR, check_in_buf, NRF_ADDR_WIDTH);
 
-//	UART_SendString(check_in_buf, NRF_ADDR_WIDTH);	
-	
-	NRF24L_Read_RegnByte(TX_ADDR, check_out_buf, NRF_ADDR_WIDTH);
-	
-//	UART_SendString(check_out_buf, NRF_ADDR_WIDTH);
+//	USART_SendString(DEBUG_UART, check_in_buf, NRF_ADDR_WIDTH);
 
-	NRF_CE(HIGH);
-	NRF24L_Delay_us(10);
-	
-	if ((check_out_buf[0] == check_in_buf[0]) && 
-			(check_out_buf[1] == check_in_buf[1]) && 
-				(check_out_buf[2] == check_in_buf[2]) && 
-					(check_out_buf[3] == check_in_buf[3]) && 
-						(check_out_buf[4] == check_in_buf[4]))
-		return 1;
-	else
-		return 0;
+    NRF24L_Read_RegnByte(NRF_TX_ADDR, check_out_buf, NRF_ADDR_WIDTH);
+
+//	USART_SendString(DEBUG_UART, check_out_buf, NRF_ADDR_WIDTH);
+
+    NRF_CE(HIGH);
+    NRF24L_Delay_us(TIME_DELAY);
+
+    if ((check_out_buf[0] == check_in_buf[0]) &&
+            (check_out_buf[1] == check_in_buf[1]) &&
+            (check_out_buf[2] == check_in_buf[2]) &&
+            (check_out_buf[3] == check_in_buf[3]) &&
+            (check_out_buf[4] == check_in_buf[4]))
+        return 1;
+    else
+        return 0;
 }
 
 /************************************************
@@ -352,9 +351,47 @@ static bit NRF24L_Check(void)
 *************************************************/
 void NRF24L_Config(void)
 {
-	NRF_CE(LOW);    	// P10 0
-	NRF_CSN(HIGH);  	// P37 6
-	NRF_SCK(LOW);  		// P36 5
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC, (FunctionalState)ENABLE);
+
+    /* Configure PC.3 pin as output push pull */
+    GPIO_InitStructure.GPIO_Pin = NRF_CSN_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+    GPIO_Init(NRF_CSN_GPIO_PORT, &GPIO_InitStructure);
+
+    /* Configure PC.6 pin as output push pull */
+    GPIO_InitStructure.GPIO_Pin = NRF_SCK_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+    GPIO_Init(NRF_SCK_GPIO_PORT, &GPIO_InitStructure);
+
+    /* Configure PB.15 pin as input pull up */
+    GPIO_InitStructure.GPIO_Pin = NRF_MISO_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(NRF_MISO_GPIO_PORT, &GPIO_InitStructure);
+
+    /* Configure PC.7 pin as output push pull */
+    GPIO_InitStructure.GPIO_Pin = NRF_MOSI_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+    GPIO_Init(NRF_MOSI_GPIO_PORT, &GPIO_InitStructure);
+
+    /* Configure PC.2 pin as output push pull */
+    GPIO_InitStructure.GPIO_Pin = NRF_CE_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+    GPIO_Init(NRF_CE_GPIO_PORT, &GPIO_InitStructure);
+
+    /* Configure PC.8 pin as input pull up */
+    GPIO_InitStructure.GPIO_Pin = NRF_IRQ_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(NRF_IRQ_GPIO_PORT, &GPIO_InitStructure);
+
+    NRF_CE(LOW);
+    NRF_CSN(HIGH);
+    NRF_SCK(LOW);
 }
 
 /************************************************
@@ -363,35 +400,52 @@ void NRF24L_Config(void)
 参    数 ： 无
 返 回 值 ： 无
 *************************************************/
-void NRF24L_Init(void)
+uint8_t NRF24L_Init(void)
 {
-	/* 工作通道接收频率 2400 + NRF_Freq MHz */
-	const uint8_t NRF_Freq = 120;				// 0 ~ 125
+    /* 工作通道接收频率 2400 + NRF_Freq MHz */
+    const uint8_t NRF_Freq = 120;				// 0 ~ 125
 
-	/* TX、RX 地址配置*/
-	const uint8_t tx_addr[5] = {0x34, 0x43, 0x10, 0x10, 0x01};			// 注意：对端的rx,tx地址跟本端交换
-	const uint8_t rx_addr[5] = {0x34, 0x43, 0x10, 0x10, 0x02};
+    /* TX、RX 地址配置*/
+    const uint8_t tx_addr[5] = {0x34, 0x43, 0x10, 0x10, 0x01};				// 注意：对端的rx,tx地址跟本端交换
+    const uint8_t rx_addr[5] = {0x34, 0x43, 0x10, 0x10, 0x02};
 
 	NRF24L_Config();
 
 	NRF24L_Delay_ms(2);
-	if(NRF24L_Check())
-	{
-		NRF_CE(LOW);
-		NRF24L_Write_RegByte(SETUP_AW, NRF_ADDR_WIDTH - 2);				// 设置地址宽度
-		NRF24L_Write_RegnByte(TX_ADDR, tx_addr, NRF_ADDR_WIDTH);		// 要发送的目的地址
-		NRF24L_Write_RegnByte(RX_ADDR_P0, rx_addr, NRF_ADDR_WIDTH);		// 自身(接收)地址
+    if(NRF24L_Check())
+    {
+        NRF_CE(LOW);
+        NRF24L_Write_RegByte(NRF_SETUP_AW, NRF_ADDR_WIDTH - 2);				// 设置地址宽度
+        NRF24L_Write_RegnByte(NRF_TX_ADDR, tx_addr, NRF_ADDR_WIDTH);		// 要发送的目的地址
+        NRF24L_Write_RegnByte(NRF_RX_ADDR_P0, rx_addr, NRF_ADDR_WIDTH);		// 自身(接收)地址
 
-		/* 射频参数要求收、发双方要一致 */
-		NRF24L_Write_RegByte(EN_AA, 0x01);								// 使能通道0的自动应答
-		NRF24L_Write_RegByte(EN_RXADDR, 0x01);							// 使能通道0的接收地址
-		NRF24L_Write_RegByte(RX_PW_P0, RX_PLOAD_WIDTH);					// 选择通道0的有效数据宽度
-		NRF24L_Write_RegByte(SETUP_RETR, 0x18);							// 设置自动重发间隔时间:500us + 86us;最大自动重发次数:8次
-		NRF24L_Write_RegByte(RF_CH, NRF_Freq);							// 设置 RF频率 = 2.4 + 0.120 GHz
-		NRF24L_Write_RegByte(RF_SETUP, 0x0F);							// 设置TX发射参数：0db增益、2Mbps、低噪声增益开启
-		NRF24L_Write_RegByte(CONFIG, 0x0F);								// 开所有中断、使能 16Bit_CRC校验、接收模式、上电
-		NRF_CE(HIGH);													// CE为高,进入接收模式
-	}
+        /* 射频参数要求收、发双方要一致 */
+        NRF24L_Write_RegByte(NRF_EN_AA, 0x00);								// 使能通道0的自动应答
+        NRF24L_Write_RegByte(NRF_EN_RXADDR, 0x01);							// 使能通道0的接收地址
+
+	#if (NRF_DYNAMIC_PACKET == 1)
+        NRF24L_Write_RegByte(NRF_DYNPD, (1 << 0)); 							// 使能通道 0动态数据长度
+        NRF24L_Write_RegByte(NRF_FEATRUE, 0x07);
+        NRF24L_Read_RegByte(NRF_DYNPD);
+        NRF24L_Read_RegByte(NRF_FEATRUE);
+
+	#elif NRF_DYNAMIC_PACKET == 0
+        NRF24L_Write_RegByte(NRF_RX_PW_P0, NRF_RX_PLOAD_WIDTH);				// 选择通道0的有效数据宽度(静态)
+
+	#endif /* NRF_DYNAMIC_PACKET */
+
+        NRF24L_Write_RegByte(NRF_SETUP_RETR, 0x18);							// 设置自动重发间隔时间:500us + 86us;最大自动重发次数:8次
+        NRF24L_Write_RegByte(NRF_RF_CH, NRF_Freq);							// 设置 RF频率 = 2.4 + 0.120 GHz
+        NRF24L_Write_RegByte(NRF_RF_SETUP, 0x0F);							// 设置TX发射参数：0db增益、2Mbps、低噪声增益开启
+        NRF24L_Write_RegByte(NRF_CONFIG, 0x0F);								// 开所有中断、使能 16Bit_CRC校验、接收模式、上电
+        NRF_CE(HIGH);														// CE为高,进入接收模式
+
+        DEBUG_PRINTF("NRF Init success...\r\n");
+        return 1;
+    }
+
+    DEBUG_PRINTF("NRF Init fail...\r\n");
+    return 0;
 }
 
 
--- NRF24L01/nrf24l01_drv.h
+++ NRF24L01/nrf24l01_drv.h
@@ -2,15 +2,15 @@
 #define __NRF24L01_H
 
 
-#include "reg52.h"
+#include "stm32f10x.h"
 
-#ifndef ENABLE
-#define ENABLE              1
-#endif /* ENABLE */
+//#ifndef ENABLE
+//#define ENABLE              1
+//#endif /* ENABLE */
 
-#ifndef DISABLE
-#define DISABLE             0
-#endif /* DISABLE */
+//#ifndef DISABLE
+//#define DISABLE             0
+//#endif /* DISABLE */
 
 #ifndef HIGH
 #define HIGH                1
@@ -21,68 +21,94 @@
 #endif /* LOW */
 
 /* ----- NRF24L01寄存器操作命令 ----- */
-#define READ_REG        0x00  //读配置寄存器,低5位为寄存器地址
-#define WRITE_REG       0x20  //写配置寄存器,低5位为寄存器地址
-#define RD_RX_PLOAD     0x61  //读RX有效数据,1~32字节
-#define WR_TX_PLOAD     0xA0  //写TX有效数据,1~32字节
-#define FLUSH_TX        0xE1  //清除TX FIFO寄存器.发射模式下用
-#define FLUSH_RX        0xE2  //清除RX FIFO寄存器.接收模式下用
-#define REUSE_TX_PL     0xE3  //重新使用上一包数据,CE为高,数据包被不断发送.
-#define NOP             0xFF  //空操作,可以用来读状态寄存器	 
+#define NRF_READ_REG        0x00  //读配置寄存器,低5位为寄存器地址
+#define NRF_WRITE_REG       0x20  //写配置寄存器,低5位为寄存器地址
+#define NRF_RD_RX_PLOAD     0x61  //读RX有效数据,1~32字节
+#define NRF_WR_TX_PLOAD     0xA0  //写TX有效数据,1~32字节
+#define NRF_FLUSH_TX        0xE1  //清除TX FIFO寄存器.发射模式下用
+#define NRF_FLUSH_RX        0xE2  //清除RX FIFO寄存器.接收模式下用
+#define NRF_REUSE_TX_PL     0xE3  //重新使用上一包数据,CE为高,数据包被不断发送.
+#define NRF_NOP             0xFF  //空操作,可以用来读状态寄存器
+/* 动态 */
+#define NRF_R_RX_PL_WID			0x60
+#define NRF_W_ACK_PLOAD			0xA8
+#define NRF_WR_TX_PLOAD_NACK	0xB0
+
 /* ----- END ----- */
 
 /* ----- NRF24L01寄存器地址 ----- */
-#define CONFIG          0x00  // 配置寄存器地址                             
-#define EN_AA           0x01  // 使能自动应答功能 
-#define EN_RXADDR       0x02  // 接收地址允许
-#define SETUP_AW        0x03  // 设置地址宽度(所有数据通道)
-#define SETUP_RETR      0x04  // 建立自动重发
-#define RF_CH           0x05  // RF通道
-#define RF_SETUP        0x06  // RF寄存器	
-#define STATUS          0x07  // 状态寄存器
-#define OBSERVE_TX      0x08  // 发送检测寄存器
-#define CD              0x09  // 载波检测寄存器
-#define RX_ADDR_P0      0x0A  // 数据通道0接收地址
-#define RX_ADDR_P1      0x0B  // 数据通道1接收地址
-#define RX_ADDR_P2      0x0C  // 数据通道2接收地址
-#define RX_ADDR_P3      0x0D  // 数据通道3接收地址
-#define RX_ADDR_P4      0x0E  // 数据通道4接收地址
-#define RX_ADDR_P5      0x0F  // 数据通道5接收地址
-#define TX_ADDR         0x10  // 发送地址寄存器
-#define RX_PW_P0        0x11  // 接收数据通道0有效数据宽度(1~32字节) 
-#define RX_PW_P1        0x12  // 接收数据通道1有效数据宽度(1~32字节) 
-#define RX_PW_P2        0x13  // 接收数据通道2有效数据宽度(1~32字节) 
-#define RX_PW_P3        0x14  // 接收数据通道3有效数据宽度(1~32字节) 
-#define RX_PW_P4        0x15  // 接收数据通道4有效数据宽度(1~32字节)
-#define RX_PW_P5        0x16  // 接收数据通道5有效数据宽度(1~32字节)
-#define FIFO_STATUS     0x17  // FIFO状态寄存器
+#define NRF_CONFIG          0x00  // 配置寄存器地址                             
+#define NRF_EN_AA           0x01  // 使能自动应答功能 
+#define NRF_EN_RXADDR       0x02  // 接收地址允许
+#define NRF_SETUP_AW        0x03  // 设置地址宽度(所有数据通道)
+#define NRF_SETUP_RETR      0x04  // 建立自动重发
+#define NRF_RF_CH           0x05  // RF通道
+#define NRF_RF_SETUP        0x06  // RF寄存器	
+#define NRF_STATUS          0x07  // 状态寄存器
+#define NRF_OBSERVE_TX      0x08  // 发送检测寄存器
+#define NRF_CD              0x09  // 载波检测寄存器
+#define NRF_RX_ADDR_P0      0x0A  // 数据通道0接收地址
+#define NRF_RX_ADDR_P1      0x0B  // 数据通道1接收地址
+#define NRF_RX_ADDR_P2      0x0C  // 数据通道2接收地址
+#define NRF_RX_ADDR_P3      0x0D  // 数据通道3接收地址
+#define NRF_RX_ADDR_P4      0x0E  // 数据通道4接收地址
+#define NRF_RX_ADDR_P5      0x0F  // 数据通道5接收地址
+#define NRF_TX_ADDR         0x10  // 发送地址寄存器
+#define NRF_RX_PW_P0        0x11  // 接收数据通道0有效数据宽度(1~32字节) 
+#define NRF_RX_PW_P1        0x12  // 接收数据通道1有效数据宽度(1~32字节) 
+#define NRF_RX_PW_P2        0x13  // 接收数据通道2有效数据宽度(1~32字节) 
+#define NRF_RX_PW_P3        0x14  // 接收数据通道3有效数据宽度(1~32字节) 
+#define NRF_RX_PW_P4        0x15  // 接收数据通道4有效数据宽度(1~32字节)
+#define NRF_RX_PW_P5        0x16  // 接收数据通道5有效数据宽度(1~32字节)
+#define NRF_FIFO_STATUS     0x17  // FIFO状态寄存器
+/* 动态 */
+#define NRF_DYNPD			0x1C  // 启用动态有效载荷长度
+#define NRF_FEATRUE			0x1D  // 
 /* ----- END ----- */
 
 /* ----- STATUS寄存器 bit位定义 ----- */
-#define TX_MAX  	0x10  	  // 达到最大发送次数中断
-#define TX_OK   	0x20  	  // TX发送完成中断
-#define RX_OK   	0x40  	  // 接收到数据中断
+#define NRF_TX_MAX  	0x10  	  // 达到最大发送次数中断
+#define NRF_TX_OK   	0x20  	  // TX发送完成中断
+#define NRF_RX_OK   	0x40  	  // 接收到数据中断
 /* ----- END ----- */
 
 /* ----- 24L01发送接收数据宽度定义 ----- */
-#define NRF_ADDR_WIDTH  5     // 5字节地址宽度
-#define TX_PLOAD_WIDTH  32    // 32字节有效数据宽度（必须与对方接收有效数据宽度一致，否则对方收不到）
-#define RX_PLOAD_WIDTH  32    // 32字节有效数据宽度
+#define NRF_ADDR_WIDTH  	5     // 5字节地址宽度
+#define NRF_TX_PLOAD_WIDTH  32    // 32字节有效数据宽度（必须与对方接收有效数据宽度一致，否则对方收不到）
+#define NRF_RX_PLOAD_WIDTH  32    // 32字节有效数据宽度
 /* ----- END ----- */
 
-#define NRF_CE(x)     P1_0 = x
-#define NRF_CSN(x)    P3_7 = x
-#define NRF_MISO   	  P2_0
-#define NRF_MOSI(x)   P2_2 = x
-#define NRF_SCK(x)    P3_6 = x
-#define NRF_IRQ       P3_3
+#define NRF_CSN_GPIO_PORT			GPIOC
+#define NRF_CSN_GPIO_PIN			GPIO_Pin_3
+
+#define NRF_SCK_GPIO_PORT			GPIOC
+#define NRF_SCK_GPIO_PIN			GPIO_Pin_6
+
+#define NRF_MISO_GPIO_PORT			GPIOB
+#define NRF_MISO_GPIO_PIN			GPIO_Pin_15
+
+#define NRF_MOSI_GPIO_PORT			GPIOC
+#define NRF_MOSI_GPIO_PIN			GPIO_Pin_7
+
+#define NRF_CE_GPIO_PORT			GPIOC
+#define NRF_CE_GPIO_PIN				GPIO_Pin_2
+
+#define NRF_IRQ_GPIO_PORT			GPIOC
+#define NRF_IRQ_GPIO_PIN			GPIO_Pin_8
+
+#define NRF_CE(x)     GPIO_WriteBit(NRF_CE_GPIO_PORT,NRF_CE_GPIO_PIN,(BitAction)x)
+#define NRF_CSN(x)    GPIO_WriteBit(NRF_CSN_GPIO_PORT,NRF_CSN_GPIO_PIN,(BitAction)x)
+#define NRF_MISO   	  GPIO_ReadInputDataBit(NRF_MISO_GPIO_PORT,NRF_MISO_GPIO_PIN)
+#define NRF_MOSI(x)   GPIO_WriteBit(NRF_MOSI_GPIO_PORT,NRF_MOSI_GPIO_PIN,(BitAction)x)
+#define NRF_SCK(x)    GPIO_WriteBit(NRF_SCK_GPIO_PORT,NRF_SCK_GPIO_PIN,(BitAction)x)
+#define NRF_IRQ       GPIO_ReadInputDataBit(NRF_IRQ_GPIO_PORT,NRF_IRQ_GPIO_PIN)
 
 void NRF24L_Write_Buf( const uint8_t *pBuf, uint8_t Len );
 void NRF24L_Read_Buf( uint8_t *pBuf, uint8_t Len );
-uint8_t NRF24L_SendPacket( uint8_t *pData, uint8_t Const );
-bit NRF24L_ReceivePacket( uint8_t *pData, uint8_t Const );
+uint8_t NRF24L_SendPacket( const uint8_t *pData, uint8_t Const );
+uint8_t NRF24L_ReceivePacket( uint8_t *pData, uint8_t Const );
 void NRF24L_Config(void);
-void NRF24L_Init(void);
+uint8_t NRF24L_Init(void);
 
 
 #endif /* __NRF24L01_H */
